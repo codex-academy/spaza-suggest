c48a846a4c0030931fcd0919441ef835951e822c
Nomzamo <nomzamo.nmuleka@gmail.com>
2022-11-23 16:46:17 +0200

    tests

---
+++ 002.tables.sql
@@ -0,0 +1,51 @@
+create table area (
+	id serial not null primary key,
+	area_name text not null
+);
+
+create table spaza (
+	id serial not null primary key,
+	shop_name text not null,
+    code text not null,
+    area_id int not null,
+    foreign key (area_id) references area(id)
+);
+
+create table spaza_client (
+	id serial not null primary key,
+	username text not null,
+    code text not null
+);
+
+create table suggestion (
+	id serial not null primary key,
+	product_name text not null,
+    area_id int not null,
+    client_id int not null,
+    foreign key (area_id) references area(id),
+    foreign key (client_id) references spaza_client(id)
+);
+
+create table accepted_suggestion(
+    id serial not null primary key,
+    suggestion_id int not null,
+    spaza_id int not null,
+    accepted_at timestamp DEFAULT NOW(),
+    foreign key (suggestion_id) references suggestion(id),
+    foreign key (spaza_id) references spaza(id)
+);
+
+-- create table liked_suggestion(
+--     id serial not null primary key,
+--     suggestion_id int not null,
+--     client_id int not null,
+--     accepted_at timestamp DEFAULT NOW(),
+--     foreign key (suggestion_id) references suggestion(id),
+--     foreign key (client_id) references spaza_client(id)
+-- );
+
+insert into area (area_name) values ('Khayelitsa - Site B');
+insert into area (area_name) values ('Khayelitsa - Site C');
+insert into area (area_name) values ('Khayelitsa - Makhaza');
+insert into area (area_name) values ('Nyanga');
+insert into area (area_name) values ('Nyanga East');
